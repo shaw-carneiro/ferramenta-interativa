5f917cbba342df65f6bb81384f3f1091a80045d3
shaw-carneiro <poli.costa@escola.pr.gov.br>
2025-07-01 13:32:57 -0300

    joguinhos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Mortal Kombat',
+    'Quais são os três ninjas principais de MK?',
+    'Scorpion, Sub zero, Noob saibot'
+)
+
+criaCartao(
+    'Minecraft',
+    'Quantos minérios de ferro formam uma armadura completa?',
+    '24'
+)
+
+criaCartao(
+    'The last of us',
+    'Qual personagem era imune aos infectados?',
+    'Ellie'
+)
+
+criaCartao(
+    'destroit',
+    'Ele e conhecido como a jogabilidade diferente qual é?',
+    'Você pode escolher o destino do jogo'
+)
